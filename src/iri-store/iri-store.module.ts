e2af118ceea4ddbcf2f24e342cdb329df49df7c4
Tom Irimescu <tom@irimescu.com>
2020-03-19 16:34:01 -0400

    Removed auth0 references

--- src/iri-store/iri-store.module.ts
+++ src/iri-store/iri-store.module.ts
@@ -1,9 +1,8 @@
-import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { IriStoreService } from './iri-store.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductSchema } from './schemas/product.schema';
 import { IriStoreController } from './iri-store.controller';
-import { AuthenticationMiddleware } from 'src/common/authentication.middleware';
 
 @Module({
     imports: [
@@ -12,14 +11,4 @@ import { AuthenticationMiddleware } from 'src/common/authentication.middleware';
     providers: [IriStoreService],
     controllers: [IriStoreController]
 })
-export class IriStoreModule implements NestModule { 
-    configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
-        consumer.apply(AuthenticationMiddleware).forRoutes(
-          { method: RequestMethod.POST, path: '/iri-store/product' },
-          { method: RequestMethod.GET, path: '/iri-store/product/:productID' },
-          { method: RequestMethod.GET, path: '/iri-store/products' },
-          { method: RequestMethod.PUT, path: '/iri-store/edit' },
-          { method: RequestMethod.DELETE, path: '/iri-store/delete' }
-        )
-      }
-}
+export class IriStoreModule{}
